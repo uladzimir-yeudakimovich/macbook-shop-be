252beae6b1bdcff0568241e5177510e8c53bc58a
ULADZIMIR YEUDAKIMOVICH <uladzimir.yeudakimovich@gmail.com>
2020-10-28 22:33:14 +0300

    refactor: product-service

--- product-service/handler.js
+++ product-service/handler.js
@@ -3,16 +3,9 @@
 module.exports.hello = async event => {
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: {
+      productName: 'Book',
+      price: 123
+    }
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
