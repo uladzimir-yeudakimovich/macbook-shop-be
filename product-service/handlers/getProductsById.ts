c4d9ac04a95deb9ecfde86f39a7325b91cddc208
ULADZIMIR YEUDAKIMOVICH <uladzimir.yeudakimovich@gmail.com>
2020-11-01 16:34:50 +0100

    feat: add try catch

--- product-service/handlers/getProductsById.ts
+++ product-service/handlers/getProductsById.ts
@@ -5,14 +5,19 @@ import productList from '../productList.json';
 export const getProductsById: APIGatewayProxyHandler = async (event) => {
   const { productId } = event.pathParameters;
   const product = await productList.find(el => el.id === productId);
-  return {
-    statusCode: product ? 200 : 404,
-    headers: {
-      "Access-Control-Allow-Headers" : "Content-Type",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET",
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(product ? product : {message: 'Product not found'})
-  };
+  try {
+    return {
+      statusCode: product ? 200 : 404,
+      headers: {
+        "Access-Control-Allow-Headers" : "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET",
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(product ? product : {message: 'Product not found'})
+    };
+  } catch (error) {
+    console.log(error);
+    throw (error);
+  }
 }
