a3a49f15a86fa0fb0ae44c2cf0720005883038f7
ULADZIMIR YEUDAKIMOVICH <uladzimir.yeudakimovich@gmail.com>
2020-11-01 16:28:30 +0100

    feat: add not found

--- product-service/handlers/getProductsById.ts
+++ product-service/handlers/getProductsById.ts
@@ -2,16 +2,17 @@ import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 import productList from '../productList.json';
 
-export const getProductsById: APIGatewayProxyHandler = async (event, _contex) => {
-  const product = await productList.find(el => el.id === event.pathParameters.productId);
+export const getProductsById: APIGatewayProxyHandler = async (event) => {
+  const { productId } = event.pathParameters;
+  const product = await productList.find(el => el.id === productId);
   return {
-    statusCode: 200,
+    statusCode: product ? 200 : 404,
     headers: {
       "Access-Control-Allow-Headers" : "Content-Type",
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "GET",
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(product)
+    body: JSON.stringify(product ? product : {message: 'Product not found'})
   };
 }
