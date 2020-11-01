c69fa53eac87b6e7b19592c9739ac2f036a91fef
ULADZIMIR YEUDAKIMOVICH <uladzimir.yeudakimovich@gmail.com>
2020-11-01 17:01:40 +0100

    feat: add try catch

--- product-service/handlers/getProductsList.ts
+++ product-service/handlers/getProductsList.ts
@@ -3,14 +3,19 @@ import 'source-map-support/register';
 import productList from '../productList.json';
 
 export const getProductsList: APIGatewayProxyHandler = async () => {
-  return {
-    statusCode: 200,
-    headers: {
-        "Access-Control-Allow-Headers" : "Content-Type",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET",
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(productList),
-  };
+  try {
+    return {
+      statusCode: 200,
+      headers: {
+          "Access-Control-Allow-Headers" : "Content-Type",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET",
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(productList),
+    };
+  } catch (error) {
+    console.log(error);
+    throw (error);
+  }
 }
