77e6ece1b7dd92971f68ece343f699b95cdd3966
ULADZIMIR YEUDAKIMOVICH <uladzimir.yeudakimovich@gmail.com>
2020-11-15 18:43:54 +0100

    fix: parser

--- src/import-service/handlers/importFileParser.js
+++ src/import-service/handlers/importFileParser.js
@@ -6,12 +6,35 @@ import { BUCKET } from '../utils/constants';
 export const importFileParser = async event => {
   console.log('importFileParser ', event);
   const s3 = new AWS.S3();
+  
+  event.Records.forEach(record => {
+    const s3Stream = s3.getObject({
+      Bucket: BUCKET,
+      Key: record.s3.object.key
+    }).createReadStream();
+
+    s3Stream.pipe(csv())
+      .on('data', data => console.log('data: ', data))
+      .on('error', error => console.error('error: ', error))
+      .on('end', async () => {
+        console.log(`Copy from ${BUCKET}/${record.s3.object.key}`);
+
+        await s3.copyObject({
+          Bucket: BUCKET,
+          CopySource: `${BUCKET}/${record.s3.object.key}`,
+          Key: record.s3.object.key.replace('uploaded', 'parsed')
+        }).promise();
+        
+        console.log(`Copy into ${BUCKET}/${record.s3.object.key.replace('uploaded', 'parsed')}`);
+      })
+  })
+
   let files;
 
   try {
     files = await s3.listObjects({
       Bucket: BUCKET,
-      Prefix: 'uploaded/',
+      Prefix: 'parsed/',
       Delimiter: '/'
     }).promise();
   } catch (error) {
@@ -22,33 +45,10 @@ export const importFileParser = async event => {
       body: JSON.stringify({ message: 'Error while reading data' })
     }
   }
-  
-  event.Records.forEach(record => {
-    const s3Stream = s3.getObject({
-      Bucket: BUCKET,
-      Key: record.s3.object.key
-    }).createReadStream();
-
-    console.log('stream: ', s3Stream);
-
-    s3Stream.pipe(csv())
-      .on('data', data => console.log('data: ', data))
-      .on('end', async () => {
-        console.log(`Copy from ${BUSKET}/${record.s3.object.key}`);
-
-        await s3.copyObject({
-          Busket: BUSKET,
-          CopySource: `${BUSKET}/${record.s3.object.key}`,
-          Key: record.s3.object.key.replace('uploaded', 'parsed')
-        }).promise();
-        
-        console.log(`Copy into ${BUSKET}/${record.s3.object.key.replace('uploaded', 'parsed')}`);
-      })
-  })
 
   return {
     statusCode: 200,
     headers: corsHeaders,
-    body: JSON.stringify(files.Contents.map(file => file.key.replace(file.Prefix, '')))
+    body: JSON.stringify(files.Contents.map(file => file.key))
   }
 }
