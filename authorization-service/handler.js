d2210f731e1b6a80b6b27116a9737cee06e745c4
ULADZIMIR YEUDAKIMOVICH <uladzimir.yeudakimovich@gmail.com>
2020-11-26 23:07:25 +0100

    feat: add basicAuthorizer

--- authorization-service/handler.js
+++ authorization-service/handler.js
@@ -1,6 +1,45 @@
-
-
-export const basicAuthorizer = async event => {
+export const basicAuthorizer = async (event, ctx, cb) => {
   console.log('basicAuthorizer: ', event);
 
+  if (event.type !== 'TOKEN') {
+    cb('Unauthorized');
+  }
+
+  try {
+    const authorizationToken = event.authorizationToken;
+
+    const encodedCreds = authorizationToken.split(' ')[1];
+    const buff = Buffer.from(encodedCreds, 'base64');
+    const plainCreds = buff.toString('utf-8').split(':');
+    const username = plainCreds[0];
+    const password = plainCreds[1];
+
+    console.log(`username: ${username} and password: ${password}`);
+
+    const storedUserPassword = process.env.uladzimiryeudakimovich;
+    const effect = !storedUserPassword || storedUserPassword !== password ? 'Deny' : 'Allow';
+
+    const policy = generatePolicy(encodedCreds, event.methodArn, effect);
+
+    cb(null, policy);
+  } catch (error) {
+    console.error('error: ', error);
+    cb('Error: Invalid token');
+  }
+}
+
+const generatePolicy = (principalId, resource, effect = 'Allow') => {
+  return {
+    principalId: principalId,
+    policyDocument: {
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Action: 'execute-api:Invoke',
+          Effect: effect,
+          Resource: resource
+        }
+      ]
+    }
+  };
 }
